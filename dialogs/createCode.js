7ceb0e4b63baee8cd97660f695eebcbbe266027f
jw84 <jerrywang@me.com>
2016-05-18 23:30:18 -0700

    better handling

--- dialogs/createCode.js
+++ dialogs/createCode.js
@@ -8,12 +8,16 @@ module.exports = {
 function addDialogs(bot) {
 	bot.add('/createCode', [
 		function (session) {
-			session.userData.inviteCode = 'N3JK29'
-			var invitecode = 'N3JK29'
+			var totalranks = '15,356'
+			var userrank = '454'
+			var getCodeMessage1 = `Yeahhh! Dope. Out of my ${totalranks} fans you're going to be #${userrank} I send the new album to.`
+			session.send(getCodeMessage1)
+
 			var pageurl = 'https://m.me/jasonderulo'
-			session.send(prompts.getCodeMessage1)
+			var invitecode = 'N3JK29'
 			var getCodeMessage2 = `To get my album even sooner and to get a better place in line, tell your friends to message me at ${pageurl} and tell them to send me your secret code: ${invitecode}`
-			session.send(prompts.getCodeMessage2)
+			
+			session.send(getCodeMessage2)
 			session.endDialog()
 		}
 	])
