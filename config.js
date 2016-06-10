518bdf7c29cd8066c7a60cfbeb12de2a12baf259
jw84 <jerrywang@me.com>
2016-06-10 10:57:03 -0700

    https and stuff

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	API_URL: 'https://launchbotapp.herokuapp.com',
-	// API_URL: 'https://9f8b4fe9.ngrok.io',
 	BOTFRAMEWORK_APPID: 'launchbottest',
 	BOTFRAMEWORK_APPSECRET: '3979895f0c004678b344d0c5da3450cb',
 }
